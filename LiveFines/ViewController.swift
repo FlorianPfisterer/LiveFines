9261b9645c3f6bae32a574ed17a8cb94df689c9f
= <=>
2016-08-17 18:03:52 +0200

    Initial Commit

---
+++ LiveFines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveFines
+//
+//  Created by Florian Pfisterer on 17.08.16.
+//  Copyright © 2016 Florian Pfisterer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
